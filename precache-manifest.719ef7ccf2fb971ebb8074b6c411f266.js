135a64705901084967a3fe16ab7d05ad3972d7d5
Ivan Bilozerov <vaniabelozerov@gmail.com>
2021-03-25 17:54:23 +0200

    Updates

---
+++ precache-manifest.719ef7ccf2fb971ebb8074b6c411f266.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "692aa1a4db77c66fba3804271ce6a55f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "df171c63963d73b20efd",
+    "url": "./static/css/2.f6a27176.chunk.css"
+  },
+  {
+    "revision": "f729b3b6d9fb39dea5b7",
+    "url": "./static/css/main.b0cfdfd3.chunk.css"
+  },
+  {
+    "revision": "df171c63963d73b20efd",
+    "url": "./static/js/2.cd70d490.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.cd70d490.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f729b3b6d9fb39dea5b7",
+    "url": "./static/js/main.4ab5be35.chunk.js"
+  },
+  {
+    "revision": "7859211cf5cc2a17445c",
+    "url": "./static/js/runtime-main.2d746ae6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2864373d6bc61c7e48f9e43b143db318.js"
+  "./precache-manifest.719ef7ccf2fb971ebb8074b6c411f266.js"
 );
 
 self.addEventListener('message', (event) => {
